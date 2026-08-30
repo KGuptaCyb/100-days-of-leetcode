186afdbe20a7c821dd69d0ca9dbb0fb55b5692a1
Khushi Gupta <gupta.khushi08091@gmail.com>
2026-08-30 12:44:02 +0530

    Add minimumDeletions method in Solution class
    Implement minimumDeletions method to calculate the minimum number of deletions required to remove both the minimum and maximum elements from the array.

---
+++ Day-82-Removing-Minimum-and-Maximum-From-Array/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int n = nums.length;
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < nums[minIndex]){
+                minIndex = i;
+            }
+            if(nums[i] > nums[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        int left = Math.min(minIndex, maxIndex);
+        int right = Math.max(minIndex, maxIndex);
+
+        int removeFront = right + 1;
+        int removeBack = n - left;
+        int removeBoth = (left + 1) + (n - right);
+
+        return Math.min(removeFront, Math.min(removeBack, removeBoth));
+    }
+}
