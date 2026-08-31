8031449df9dedceb5c4fc1b4060febea35a4d12d
Khushi Gupta <gupta.khushi08091@gmail.com>
2026-08-31 20:27:32 +0530

    Implement method to find nodes between critical points

---
+++ Day-83-Find-Minimum-and-Maximum-Number-of-Nodes-Between-Critical-Points/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+
+        int first = -1;
+        int previous = -1;
+        int minDistance = Integer.MAX_VALUE;
+        int index = 1;
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while (curr != null && curr.next != null) {
+            if ((curr.val > prev.val && curr.val > curr.next.val) ||
+                (curr.val < prev.val && curr.val < curr.next.val)) {
+
+                if (first == -1) {
+                    first = index;
+                } else {
+                    minDistance = Math.min(minDistance, index - previous);
+                }
+
+                previous = index;
+            }
+
+            prev = curr;
+            curr = curr.next;
+            index++;
+        }
+        if (first == -1 || previous == first) {
+            return new int[]{-1, -1};
+        }
+        int maxDistance = previous - first;
+
+        return new int[]{minDistance, maxDistance};
+    }
+}
